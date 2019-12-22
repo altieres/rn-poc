277072fb4c7d2cdeceb2d3179b9d143a6f2a6390
Altieres Lopes <altiereslopes@webgoal.com.br>
2019-12-22 16:38:22 -0300

    expo ejected

--- src/PlayerScreen.tsx
+++ src/PlayerScreen.tsx
@@ -5,6 +5,51 @@ import { Audio } from 'expo-av';
 import { DefaultContainer, CoverImage, DefaultText } from './components';
 import { MaterialIcons } from '@expo/vector-icons';
 
+// require the module
+var RNFS = require('react-native-fs');
+
+// create a path you want to write to
+// :warning: on iOS, you cannot write into `RNFS.MainBundlePath`,
+// but `RNFS.DocumentDirectoryPath` exists on both platforms and is writable
+var path = RNFS.DocumentDirectoryPath + '/test.txt';
+
+console.log(RNFS.DocumentDirectoryPath + '/test.txt');
+console.log(RNFS.MainBundlePath);
+
+
+RNFS.readDir(RNFS.DocumentDirectoryPath + '/../..') // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+  .then((result) => {
+    console.log('GOT RESULT', result);
+
+    // stat the first file
+    return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+  })
+  .then((statResult) => {
+    if (statResult[0].isFile()) {
+      // if we have a file, read it
+      return RNFS.readFile(statResult[1], 'utf8');
+    }
+
+    return 'no file';
+  })
+  .then((contents) => {
+    // log the file contents
+    console.log(contents);
+  })
+  .catch((err) => {
+    console.log(err.message, err.code);
+  });
+
+
+// import jsmediatags from 'jsmediatags';
+
+// jsmediatags.read("/sdcard/Download/burguesinha.mp3", {
+//   onSuccess: function(tag) {
+//     var tags = tag.tags;
+//     alert(tags.artist + " - " + tags.title + ", " + tags.album);
+//   }
+// });
+
 const play = async () => {
   try {
     const { sound: soundObject, status } = await Audio.Sound.createAsync(
